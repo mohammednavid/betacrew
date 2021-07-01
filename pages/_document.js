4697a30a3b47e729f30c7377e728e9c812585f6f
mohammednavid <navidmohd5@gmail.com>
2021-07-01 11:28:27 +0530

    full frontend

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { GA_TRACKING_ID } from "../lib/gtag";
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 
-const TawkWithNoSSR = dynamic(
-  () => import('../components/Chat'),
-  { ssr: false }
-  )
+const TawkWithNoSSR = dynamic(() => import("../components/Chat"), {
+  ssr: false,
+});
 export default class MyDocument extends Document {
-
   render() {
-
     return (
       <Html>
         <Head>
@@ -38,33 +35,32 @@ export default class MyDocument extends Document {
             }}
           />
 
-<link rel="icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#24509b" />
-        <meta name="msapplication-TileColor" content="#ffc40d" />
-        <meta name="theme-color" content="#ffffff" />
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#24509b" />
+          <meta name="msapplication-TileColor" content="#ffc40d" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
-          <Main>
-            <TawkWithNoSSR/>
-          </Main>
+          <Main />
+          <TawkWithNoSSR />
           <NextScript />
         </body>
       </Html>
