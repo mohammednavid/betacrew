cbda9fd23b20cbadb0601c7c183fa7593e1d7193
mohammednavid <navidmohd5@gmail.com>
2021-07-05 12:27:19 +0530

    full frontend

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,11 +9,37 @@ const Header = () => {
     <nav className={styles.header}>
       {menu ? <Menu menu={menu} setMenu={setMenu} /> : ""}
       <Link href="/">
-        <img src="/images/logo.svg" alt="Betacrew" className={styles.headerLogo} />
+        <img
+          src="/images/logo.svg"
+          alt="Betacrew"
+          className={styles.headerLogo}
+        />
       </Link>
       <div onClick={(menu) => setMenu(true)} className={styles.headerMenu}>
         <p>Menu</p>
-        <img src="/images/Burgermenu.svg" alt="Betacrew" width={30} height={30} />
+        <img
+          src="/images/Burgermenu.svg"
+          alt="Betacrew"
+          width={30}
+          height={30}
+        />
+      </div>
+      <div className={styles.headerDesktop}>
+        <Link href="/our-work">
+          <a className={styles.headerLink}>Work</a>
+        </Link>
+        <Link href="/how-it-works">
+          <a className={styles.headerLink}>How it works</a>
+        </Link>
+        <Link href="/about">
+          <a className={styles.headerLink}>About</a>
+        </Link>
+        <Link href="/contact">
+          <a className={styles.headerLink}>Contact</a>
+        </Link>
+        <Link href="/contact">
+          <a className={styles.headerButton}>Email</a>
+        </Link>
       </div>
     </nav>
   );
